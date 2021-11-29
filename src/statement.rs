5eb2c2ecb9f9602e5d8f66324f9335b80e0f9603
Thierry Arnoux <thierry.arnoux@gmx.net>
2021-11-29 10:07:05 +0800

    Handle links and paragraphs in statement comments.

--- src/statement.rs
+++ src/statement.rs
@@ -1,7 +1,7 @@
 use handlebars::Handlebars;
 use metamath_knife::parser::as_str;
 use metamath_knife::Database;
-use regex::Regex;
+use regex::{Captures, Regex};
 use serde::Serialize;
 use std::sync::Arc;
 
@@ -20,21 +20,27 @@ struct PageInfo {
     steps: Vec<StepInfo>,
 }
 
+trait Replacer: FnMut(&Captures) -> String + Sized + Clone {}
+
 #[derive(Clone)]
 pub struct Renderer {
     templates: Arc<Handlebars<'static>>,
     db: Database,
+    link_regex: Regex,
 }
 
 impl Renderer {
     pub(crate) fn new(db: Database) -> Renderer {
         let mut templates = Handlebars::new();
+        templates.register_escape_fn(handlebars::no_escape);
         templates
             .register_template_string("statement", include_str!("statement.hbs"))
             .expect("Unable to parse statement template.");
+        let link_regex = Regex::new(r"\~ ([^ ]+) ").unwrap();
         Renderer {
             templates: Arc::new(templates),
             db,
+            link_regex,
         }
     }
 
@@ -46,10 +52,17 @@ impl Renderer {
             let mut span = cmt.span();
             span.start += 2;
             span.end -= 3;
-            Regex::new(r"\n +").unwrap().replace(
-                &String::from_utf8_lossy(span.as_ref(&cmt.segment().segment.buffer)),
-                "\n  ",
-            )
+            let comment = String::from_utf8_lossy(span.as_ref(&cmt.segment().segment.buffer))
+                .replace("\n\n", "</p>\n<p>");
+            self.link_regex
+                .replace_all(comment.as_str(), |caps: &Captures| {
+                    format!(
+                        "<a href=\"{}\">{}</a>",
+                        caps.get(1).map_or("#", |m| m.as_str()),
+                        caps.get(1).map_or("-", |m| m.as_str())
+                    )
+                })
+                .to_string()
         } else {
             "(This statement does not have an associated comment)".to_string()
         };
